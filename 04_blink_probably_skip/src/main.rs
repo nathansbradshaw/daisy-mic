db1e604d07522a1826690b939ac2853c007cf918
Nathan Bradshaw <nathansbradshaw@gmail.com>
2025-08-11 21:16:21 -0500

    init

---
+++ 04_blink_probably_skip/src/main.rs
@@ -0,0 +1,69 @@
+#![no_main]
+#![no_std]
+
+#[rtic::app(
+    device = stm32h7xx_hal::stm32,
+    peripherals = true
+)]
+mod app {
+    // Includes a panic handler and optional logging facilities
+    use libdaisy::{gpio, logger, system};
+    use log::info;
+    use stm32h7xx_hal::{stm32, time::MilliSeconds, timer::Timer};
+
+    #[shared]
+    struct Shared {}
+
+    #[local]
+    struct Local {
+        seed_led: gpio::SeedLed,
+        timer2: Timer<stm32::TIM2>,
+    }
+
+    #[init]
+    fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        logger::init();
+        let mut core = ctx.core;
+        let device = ctx.device;
+        let ccdr = system::System::init_clocks(device.PWR, device.RCC, &device.SYSCFG);
+        let system = libdaisy::system_init!(core, device, ccdr);
+        info!("Startup done!");
+        let mut timer2 = stm32h7xx_hal::timer::TimerExt::timer(
+            device.TIM2,
+            MilliSeconds::from_ticks(100).into_rate(),
+            ccdr.peripheral.TIM2,
+            &ccdr.clocks,
+        );
+        timer2.listen(stm32h7xx_hal::timer::Event::TimeOut);
+
+        timer2.set_freq(MilliSeconds::from_ticks(500).into_rate());
+
+        (
+            Shared {},
+            Local {
+                seed_led: system.gpio.led,
+                timer2,
+            },
+            init::Monotonics(),
+        )
+    }
+
+    #[idle]
+    fn idle(_cx: idle::Context) -> ! {
+        loop {
+            cortex_m::asm::nop();
+        }
+    }
+
+    #[task(binds = TIM2, local = [timer2, seed_led, led_is_on: bool = true])]
+    fn blink(ctx: blink::Context) {
+        ctx.local.timer2.clear_irq();
+
+        if *ctx.local.led_is_on {
+            ctx.local.seed_led.set_high();
+        } else {
+            ctx.local.seed_led.set_low();
+        }
+        *ctx.local.led_is_on = !(*ctx.local.led_is_on);
+    }
+}
---
+++ 05_pass_through/src/main.rs
@@ -0,0 +1,60 @@
+#![no_main]
+#![no_std]
+#[rtic::app(
+    device = stm32h7xx_hal::stm32,
+    peripherals = true,
+)]
+mod app {
+    const BLOCK_SIZE: usize = 128;
+    use libdaisy::logger;
+    use libdaisy::{audio, system};
+    use log::info;
+
+    // Shared resources (shared across tasks)
+    #[shared]
+    struct Shared {}
+
+    // Local resources (specific to a task)
+    #[local]
+    struct Local {
+        audio: audio::Audio,
+    }
+
+    // Initialization function
+    #[init]
+    fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        logger::init();
+
+        let mut core = ctx.core;
+        let device = ctx.device;
+        let ccdr = system::System::init_clocks(device.PWR, device.RCC, &device.SYSCFG);
+        let system = libdaisy::system_init!(core, device, ccdr, BLOCK_SIZE);
+
+        info!("Startup done!!");
+
+        (
+            Shared {},
+            Local {
+                audio: system.audio,
+            },
+            init::Monotonics(),
+        )
+    }
+
+    // Non-default idle ensures chip doesn't go to sleep which causes issues for
+    // probe.rs currently
+    #[idle]
+    fn idle(_ctx: idle::Context) -> ! {
+        loop {
+            cortex_m::asm::nop();
+        }
+    }
+
+    // Interrupt handler for audio
+    #[task(binds = DMA1_STR1, local = [audio], priority = 8)]
+    fn audio_handler(ctx: audio_handler::Context) {
+        let audio = ctx.local.audio;
+
+        audio.for_each(|left, right| (left, right));
+    }
+}
